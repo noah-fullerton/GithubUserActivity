8f385f0a1940559a64b29d97f1e3ab51da926ff3
noah-fullerton <noahmfullerton@gmail.com>
2024-10-01 20:09:46 -0600

    basic functionality working of printing what user did to repo and to what repo

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 import typer
+import requests
 
 app = typer.Typer()
 
 def main(username: str):
     pass
 
+def get_events(username: str):
+    response = requests.get(f"https://api.github.com/users/{username}/events")
+    events = response.json()
+    return events
+
+def format_events(events):
+    formattedEvents = []
+    for event in events:
+        formattedEvents.append(f"{eventToString(event['type'])} {event['repo']['name']}")
+    return formattedEvents
+
+def eventToString(event):
+    switcher = {
+        "PushEvent": "Pushed to",
+        "CreateEvent": "Created",
+        "DeleteEvent": "Deleted",
+        "ForkEvent": "Forked",
+        "PullRequestEvent": "Opened a pull request in",
+        "IssuesEvent": "Opened an issue in",
+        "IssueCommentEvent": "Commented on an issue in",
+        "WatchEvent": "Starred",
+        "ReleaseEvent": "Released"
+    }
+    return switcher.get(event, "Invalid event")
+
+def print_events(events):
+    for event in events:
+        print(event)
+
 if __name__ == "__main__":
-    typer.run(main)
+    print_events(format_events(get_events("noah-fullerton")))
+    #typer.run(main)
